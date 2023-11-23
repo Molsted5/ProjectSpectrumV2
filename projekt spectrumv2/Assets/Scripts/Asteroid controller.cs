2092b4935fe9034eeaea2d2b4676b2620da38e74
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2023-11-23 17:02:04 +0100

    drop script updated

--- projekt spectrumv2/Assets/Scripts/Asteroid controller.cs
+++ projekt spectrumv2/Assets/Scripts/Asteroid controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Asteroidcontroller : MonoBehaviour
 {
@@ -9,16 +10,36 @@ public class Asteroidcontroller : MonoBehaviour
     public float KnockbackStreangth;
     public float AsteroidHealth;
     public GameObject Collectable;
+    Vector3 test;
+    Transform shipTransform;
+    public CapsuleCollider CC;
+    public float offset = 1.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        test = new Vector3(110f, 110f, 110f);
         rb = GetComponent<Rigidbody>();
+        
+
+
+    }
+
+    Vector3 CalculatedPosition()
+    {
+        shipTransform = GameObject.Find("Player2(done)").GetComponent<Rigidbody>().transform;
+        Vector3 shipPosition = shipTransform.position;
+        Vector3 asteroidePosition = rb.transform.position;
+
+        Vector3 dir = (shipPosition - asteroidePosition).normalized;
+        Vector3 pos = this.CC.radius * (dir * offset);
+        return pos;
     }
 
     private void OnCollisionEnter(Collision other)   //detect collision
     {
+        
         if (other.gameObject.CompareTag("Bullet"))   //detect if tag on collided object is "Bullet"
         {
             rb.AddForce(Vector3.up * KnockbackStreangth, ForceMode.Impulse);   //Add force equal depending on set knockback streangth. Is also affected by object mass.
@@ -30,12 +51,21 @@ public class Asteroidcontroller : MonoBehaviour
             if (AsteroidHealth <= 10)
             {
                 AsteroidHealth--;
-                Instantiate(Collectable, Vector3.back, Quaternion.identity);
+            }
+
+            if (AsteroidHealth == 4)
+            {
+               GameObject dropInstance = Instantiate(Collectable, CalculatedPosition(), Quaternion.identity);
+            }   
+
+            if (AsteroidHealth == 3)
+            {
+                GameObject dropInstance = Instantiate(Collectable, CalculatedPosition(), Quaternion.identity);
             }
 
             if (AsteroidHealth < 1)
             {
-                Instantiate(Collectable, gameObject.transform);
+                Instantiate(Collectable, CalculatedPosition(), Quaternion.identity);
                 Destroy(gameObject);
                 
             }
