bdc1a030b7fe0374c56d3294a01f8b8f3c93b998
Jonas Molsted <jonasmolsted@gmail.com>
2023-11-26 15:04:21 +0100

    PlayerController script and values changed/added

--- projekt spectrumv2/Assets/Scripts/PlayerController.cs
+++ projekt spectrumv2/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,15 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public int playernumber = 1;
-    public float speedDefault = 70;
-    public float angularSpeedDefault = 240;
-    public float angularDampingDefault = 20;
+    public float TargetSpeed = 70f;
+    public float initialSpeed = 120f;
+    public float angularSpeedDefault = 240f;
+    public float angularDampingDefault = 20f;
     public float maxDriftSpeed = 0.8f;
     public float t = 0.2f;
     public float t2 = 1f;
-    public float invertTurningTreshold = -10f;
-   
+    public float p = 1f;
+    public float p2 = 1f;
 
     Rigidbody rb;
     float moveInputValue;
@@ -33,9 +34,12 @@ public class PlayerController : MonoBehaviour
         float localForwardSign = Mathf.Sign(moveInputValue); // forward = 1 and backward = -1
         
         float currentSpeed = currentVelocity.magnitude;
-        float alphaAngularSpeed  = Mathf.InverseLerp(t, t2, currentSpeed / speedDefault); // value-line stretched
-        float angularSpeed = Mathf.Lerp(angularSpeedDefault, angularDampingDefault, Mathf.Clamp(alphaAngularSpeed, 0, 1)); // angularSpeed dependent on current speed
-        
+        float alphaAngularSpeed  = Mathf.Clamp(Mathf.Pow(Mathf.InverseLerp(t, t2, currentSpeed / TargetSpeed),p2), 0, 1); // value-line stretched
+        float angularSpeed = Mathf.Lerp(angularSpeedDefault, angularDampingDefault, alphaAngularSpeed); // angularSpeed dependent on current speed
+
+        float alphaMovementSpeed = Mathf.Clamp(Mathf.Pow(currentSpeed / TargetSpeed, p), 0, 1);
+        float movementSpeed = Mathf.Lerp(initialSpeed, TargetSpeed, alphaMovementSpeed);
+
         // Drift compensation to not loose controll, when turning
         float driftCompensation;
 
@@ -50,16 +54,18 @@ public class PlayerController : MonoBehaviour
             driftCompensation = Mathf.Lerp(1, 2, alphaDrift);
         }
 
+
+
         // Add forces to move
         moveInputValue = Input.GetAxis("Vertical" + playernumber); // [-1;1] back/forward input
-        rb.AddRelativeForce(Vector3.forward * moveInputValue * speedDefault);
+        rb.AddRelativeForce(Vector3.forward * moveInputValue * movementSpeed);
 
         angularInputValue = Input.GetAxis("Horizontal" + playernumber); // [-1;1] left/right input
         rb.AddRelativeTorque(Vector3.up * angularInputValue * angularSpeed * driftCompensation * localForwardSign);
 
-        //Debug.Log(rb.angularVelocity.magnitude);
-        Debug.Log(driftCompensation);
-        //Debug.Log(localVelocity.z); 
+        Debug.Log(rb.angularVelocity.magnitude);
+        //Debug.Log(driftCompensation);
+        //Debug.Log(currentSpeed); 
     }
 
 }
