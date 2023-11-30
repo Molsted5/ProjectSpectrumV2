574e1e5c9a5ba838ff428a8f19b9386a7a2d37ab
Jonas Molsted <jonasmolsted@gmail.com>
2023-11-30 16:59:51 +0100

    Movement code rewritten with boost

---
+++ projekt spectrumv2/Assets/Scripts/Legacy/PlayerControllerLegacy.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControllerLegacy : MonoBehaviour
+{
+    public int playernumber = 1;
+    public float TargetSpeed = 70f;
+    public float initialSpeed = 120f;
+    public float angularSpeedDefault = 240f;
+    public float angularDampingDefault = 20f;
+    public float maxDriftSpeed = 0.8f;
+    public float t = 0.2f;
+    public float t2 = 1f;
+    public float p = 1f;
+    public float p2 = 1f;
+
+    Rigidbody rb;
+    float moveInputValue;
+    float angularInputValue;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Finds and saves the desired angular speed 
+        Vector3 currentVelocity = rb.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(currentVelocity); // using local transform
+        float localForwardSign = Mathf.Sign(moveInputValue); // forward = 1 and backward = -1
+        
+        float currentSpeed = currentVelocity.magnitude;
+        float alphaAngularSpeed  = Mathf.Clamp(Mathf.Pow(Mathf.InverseLerp(t, t2, currentSpeed / TargetSpeed),p2), 0, 1); // value-line stretched
+        float angularSpeed = Mathf.Lerp(angularSpeedDefault, angularDampingDefault, alphaAngularSpeed); // angularSpeed dependent on current speed
+
+        float alphaMovementSpeed = Mathf.Clamp(Mathf.Pow(currentSpeed / TargetSpeed, p), 0, 1);
+        float movementSpeed = Mathf.Lerp(initialSpeed, TargetSpeed, alphaMovementSpeed);
+
+        // Drift compensation to not loose controll, when turning
+        float driftCompensation;
+
+        if (Mathf.Sign(angularInputValue) * localForwardSign == Mathf.Sign(rb.angularVelocity.y)) // checks if input matches direction
+        {
+            driftCompensation = 1; // Nothing changes
+        }
+        else
+        {
+            float currentAngularSpeed = rb.angularVelocity.magnitude;
+            float alphaDrift = Mathf.Clamp(currentAngularSpeed / maxDriftSpeed, 0, 1);
+            driftCompensation = Mathf.Lerp(1, 2, alphaDrift);
+        }
+
+
+
+        // Add forces to move
+        moveInputValue = Input.GetAxis("Vertical" + playernumber); // [-1;1] back/forward input
+        rb.AddRelativeForce(Vector3.forward * moveInputValue * movementSpeed);
+
+        angularInputValue = Input.GetAxis("Horizontal" + playernumber); // [-1;1] left/right input
+        rb.AddRelativeTorque(Vector3.up * angularInputValue * angularSpeed * driftCompensation * localForwardSign);
+
+        //Debug.Log(rb.angularVelocity.magnitude);
+        //Debug.Log(driftCompensation);
+        //Debug.Log(currentSpeed); 
+    }
+
+}
--- projekt spectrumv2/Assets/Scripts/PlayerController.cs
+++ projekt spectrumv2/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public int playernumber = 1;
-    public float TargetSpeed = 70f;
+    public float playerNumber = 1f;
+    public float targetSpeed = 70f;
     public float initialSpeed = 120f;
     public float angularSpeedDefault = 240f;
     public float angularDampingDefault = 20f;
@@ -16,56 +16,72 @@ public class PlayerController : MonoBehaviour
     public float t2 = 1f;
     public float p = 1f;
     public float p2 = 1f;
+    public float boostScale = 1.5f;
 
     Rigidbody rb;
-    float moveInputValue;
-    float angularInputValue;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    float MoveInputValue( float playerNumber ) {    // [-1;1] back/forward input
+        return Input.GetAxis( "Vertical" + playerNumber ); 
+    }
+
+    float AngularInputValue( float playerNumber ) { // [-1;1] left/right input
+        return Input.GetAxis( "Horizontal" + playerNumber );
+    }
+
+    float LocalForwardSign() {  // forward = 1 and backward = -1
+        return Mathf.Sign( MoveInputValue( playerNumber ) ); 
+    }
+
+    float MovementSpeed( float initialSpeed, float targetSpeed, Rigidbody rb, float p ) {
+        float alphaMovementSpeed = Mathf.Clamp( Mathf.Pow( rb.velocity.magnitude / targetSpeed, p ), 0, 1 );
+        return Mathf.Lerp( initialSpeed, targetSpeed, alphaMovementSpeed );
+    }
+
+    float AngularSpeed( float angularSpeedDefault, float angularDampingDefault, float targetSpeed, Rigidbody rb, float t, float t2, float p2 ) {
+        float alphaAngularSpeed = Mathf.Clamp(Mathf.Pow(Mathf.InverseLerp(t, t2, rb.velocity.magnitude / targetSpeed), p2), 0, 1); // value-line stretched
+        return Mathf.Lerp(angularSpeedDefault, angularDampingDefault, alphaAngularSpeed); // angularSpeed dependent on current speed
+    }
+
+    float DriftCompensation( float maxDriftSpeed, Rigidbody rb ) { // drift compensation to not loose controll, when turning
+        float driftCompensation;
+        if ( Mathf.Sign( AngularInputValue( playerNumber ) ) * LocalForwardSign() == Mathf.Sign( rb.angularVelocity.y ) ) // checks if input matches direction
+        {
+            driftCompensation = 1; // nothing changes
+        }
+        else {
+            float alphaDrift = Mathf.Clamp( rb.angularVelocity.magnitude / maxDriftSpeed, 0, 1 );
+            driftCompensation = Mathf.Lerp( 1, 2, alphaDrift );
+        }
+        return driftCompensation; 
+    }
+
+    float MovementBoost( float boostScale, float playerNumber ) {
+        float res;
+        if( Input.GetButton( "Boost" + playerNumber ) ) {
+            res = boostScale;
+        }
+        else {
+            res = 1f;
+        }
+        return res;
+    }
+
     void FixedUpdate()
     {
-        // Finds and saves the desired angular speed 
-        Vector3 currentVelocity = rb.velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(currentVelocity); // using local transform
-        float localForwardSign = Mathf.Sign(moveInputValue); // forward = 1 and backward = -1
-        
-        float currentSpeed = currentVelocity.magnitude;
-        float alphaAngularSpeed  = Mathf.Clamp(Mathf.Pow(Mathf.InverseLerp(t, t2, currentSpeed / TargetSpeed),p2), 0, 1); // value-line stretched
-        float angularSpeed = Mathf.Lerp(angularSpeedDefault, angularDampingDefault, alphaAngularSpeed); // angularSpeed dependent on current speed
+        // add forces to move
 
-        float alphaMovementSpeed = Mathf.Clamp(Mathf.Pow(currentSpeed / TargetSpeed, p), 0, 1);
-        float movementSpeed = Mathf.Lerp(initialSpeed, TargetSpeed, alphaMovementSpeed);
+        rb.AddRelativeForce( Vector3.forward * MoveInputValue( playerNumber ) * MovementSpeed( initialSpeed, targetSpeed, rb, p ) * MovementBoost( boostScale, playerNumber ) );
 
-        // Drift compensation to not loose controll, when turning
-        float driftCompensation;
-
-        if (Mathf.Sign(angularInputValue) * localForwardSign == Mathf.Sign(rb.angularVelocity.y)) // checks if input matches direction
-        {
-            driftCompensation = 1; // Nothing changes
-        }
-        else
-        {
-            float currentAngularSpeed = rb.angularVelocity.magnitude;
-            float alphaDrift = Mathf.Clamp(currentAngularSpeed / maxDriftSpeed, 0, 1);
-            driftCompensation = Mathf.Lerp(1, 2, alphaDrift);
-        }
-
-
-
-        // Add forces to move
-        moveInputValue = Input.GetAxis("Vertical" + playernumber); // [-1;1] back/forward input
-        rb.AddRelativeForce(Vector3.forward * moveInputValue * movementSpeed);
-
-        angularInputValue = Input.GetAxis("Horizontal" + playernumber); // [-1;1] left/right input
-        rb.AddRelativeTorque(Vector3.up * angularInputValue * angularSpeed * driftCompensation * localForwardSign);
+        rb.AddRelativeTorque( Vector3.up * AngularInputValue( playerNumber ) * AngularSpeed( angularSpeedDefault, angularDampingDefault, targetSpeed, rb, t, t2, p2 ) * DriftCompensation( maxDriftSpeed, rb ) * LocalForwardSign() );
 
         //Debug.Log(rb.angularVelocity.magnitude);
         //Debug.Log(driftCompensation);
-        //Debug.Log(currentSpeed); 
+        Debug.Log(rb.velocity.magnitude); 
     }
 
 }
