773e999dd059e7f283777299c7f087851abfed76
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2023-12-01 12:20:42 +0100

    player 2 collision fix

--- projekt spectrumv2/Assets/Scripts/IgnoreLayer - Copy.cs
+++ projekt spectrumv2/Assets/Scripts/IgnoreLayer - Copy.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class IgnoreLayer : MonoBehaviour
 {
+
     void Start()
     {
-        Physics.IgnoreLayerCollision(6, 6);
+        Physics.IgnoreLayerCollision(6, 8);
         Physics.IgnoreLayerCollision(6, 7);
+        Physics.IgnoreLayerCollision(8, 7);
+        Physics.IgnoreLayerCollision(8, 9); 
     }
 }
