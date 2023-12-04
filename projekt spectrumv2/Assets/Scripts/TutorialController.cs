945ea91b18f345eb7aac5f4d5c73a971bf97e3ae
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2023-12-04 16:17:25 +0100

    Revert "Merge branch 'main' of https://github.com/Molsted5/ProjectSpectrumV2"
    This reverts commit d740c2092f7aac77e8fe09066f5e1858ba6742e3, reversing
    changes made to f27e619737b51583b639a1edc480eaf54a26d587.

--- projekt spectrumv2/Assets/Scripts/TutorialController.cs
+++ projekt spectrumv2/Assets/Scripts/TutorialController.cs
@@ -16,10 +16,10 @@ public class TutorialController : MonoBehaviour
     private GameObject PlayerComponent;
     private GameObject ControlsComponent;
     private GameObject ShootComponent;
-    private GameObject ObjectiveComponent;    
+    private GameObject ObjectiveComponent;
+    private GameObject HealthObjective;
     public GameObject ShipText;
     public GameObject ContinueText;
-    public GameObject OutroText;
 
     public GameObject Player1;
     public GameObject Player2;
@@ -63,10 +63,11 @@ public class TutorialController : MonoBehaviour
         ShootComponent = GameObject.Find("Shoot_Intro");
         ShootComponent.SetActive(false);
         ObjectiveComponent = GameObject.Find("Objective_Intro");
-        ObjectiveComponent.SetActive(false);       
+        ObjectiveComponent.SetActive(false);
+        HealthObjective = GameObject.Find("Health_Intro");
+        HealthObjective.SetActive(false);
         ShipText.SetActive(false);
-        //ContinueText.SetActive(false);
-        OutroText.SetActive(false);
+        ContinueText.SetActive(false);
 
         P1Position = Player1.transform.position;
         P1Rotation = Player1.transform.rotation;
@@ -115,7 +116,7 @@ public class TutorialController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
         if (Input.GetKeyDown(KeyCode.K))
         {           
             switch (StageCount)
@@ -144,8 +145,7 @@ public class TutorialController : MonoBehaviour
                     Player1.GetComponent<PlayerController>().enabled = false;
                     Player2.GetComponent<PlayerController>().enabled = false;
                     ControlsComponent.SetActive(false);
-                    Player1.GetComponent<player1ShootTest>().enabled = true;
-                    Player2.GetComponent<player2ShootTest>().enabled = true;
+
                     ShootComponent.SetActive(true);
                     StageCount++;
                     break;
@@ -156,10 +156,7 @@ public class TutorialController : MonoBehaviour
                     break;
                 case 5:
                     ObjectiveComponent.SetActive(false);
-                    Player1.SetActive(false);
-                    Player2.SetActive(false);
-                    PlayerComponent.SetActive(false);
-                    OutroText.SetActive(true);
+                    HealthObjective.SetActive(true);
                     StageCount++;
                     break;
 
